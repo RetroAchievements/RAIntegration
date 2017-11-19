0676ec4cee804af934a2e243e0bfbdcecea5a9d4
GameDragon2k <gdragon2k@yahoo.com>
2017-11-19 02:52:13 -0500

    Temp. remove "Unknown" from RP parses.

--- RA_RichPresence.cpp
+++ RA_RichPresence.cpp
@@ -13,7 +13,7 @@ const std::string& RA_Lookup::Lookup( DataPos nValue ) const
 	if( m_lookupData.find( nValue ) != m_lookupData.end() )
 		return m_lookupData.find( nValue )->second;
 
-	static const std::string sUnknown = "Unknown";
+	static const std::string sUnknown = "";
 	return sUnknown;
 }
 

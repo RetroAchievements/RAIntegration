2d79d1fe0efbcf2a06e9b5161b116a940957aaa5
Jamiras <jamirasmt@gmail.com>
2018-04-29 10:45:00 -0600

    eliminate redundant Reset in Achievement.Test

--- src/RA_Achievement.cpp
+++ src/RA_Achievement.cpp
@@ -137,16 +137,11 @@ BOOL Achievement::Test()
 
 	if( bResetConditions && bNotifyOnReset )
 	{
-		Reset();
+		RA_CausePause();
 
-		if ( GetPauseOnReset() )
-		{
-			RA_CausePause();
-
-			char buffer[256];
-			sprintf_s( buffer, 256, "Pause on Reset: %s", Title().c_str() );
-			MessageBox( g_RAMainWnd, NativeStr(buffer).c_str(), TEXT("Paused"), MB_OK );
-		}
+		char buffer[256];
+		sprintf_s( buffer, 256, "Pause on Reset: %s", Title().c_str() );
+		MessageBox( g_RAMainWnd, NativeStr(buffer).c_str(), TEXT("Paused"), MB_OK );
 	}
 
 	return bRetVal;

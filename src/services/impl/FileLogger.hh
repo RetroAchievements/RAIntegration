f5bdd9df487713da6ab3700b8e7d07cb7f347573
Jamiras <jamirasmt@gmail.com>
2018-10-06 07:06:38 -0600

    use provided function for writing newline

--- src/services/impl/FileLogger.hh
+++ src/services/impl/FileLogger.hh
@@ -23,7 +23,7 @@ public:
     {
         m_pWriter = pFileSystem.AppendTextFile(pFileSystem.BaseDirectory() + L"RACache\\RALog.txt");
         if (m_pWriter != nullptr)
-            m_pWriter->Write("\r\n");
+            m_pWriter->WriteLine();
     }
 
     bool IsEnabled([[maybe_unused]] LogLevel level) const override
@@ -65,7 +65,7 @@ public:
             m_pWriter->Write(sMessage);
 
             // newline
-            m_pWriter->Write("\r\n");
+            m_pWriter->WriteLine();
         }
 
         // if writing to a file, flush immediately

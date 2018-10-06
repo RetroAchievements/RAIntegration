4376130822c883e5a0d13e84216c6316c24c0c60
Jamiras <jamirasmt@gmail.com>
2018-10-06 07:18:41 -0600

    force timezone for unit tests

--- tests/services/FileLogger_Tests.cpp
+++ tests/services/FileLogger_Tests.cpp
@@ -41,9 +41,9 @@ public:
         logger.LogMessage(LogLevel::Error, "This is the third message.");
 
         Assert::AreEqual(std::string("\r\n"
-            "160843.000|INFO| This is a message.\r\n"
-            "160843.000|WARN| This is another message.\r\n"
-            "160843.375|ERR | This is the third message.\r\n"), mockFileSystem.GetFileContents(mockLogFileName));
+            "220843.000|INFO| This is a message.\r\n"
+            "220843.000|WARN| This is another message.\r\n"
+            "220843.375|ERR | This is the third message.\r\n"), mockFileSystem.GetFileContents(mockLogFileName));
     }
 };
 

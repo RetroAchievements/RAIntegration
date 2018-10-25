4b920899ebb452e277a2fe7f186924cff18294d6
Jamiras <32680403+Jamiras@users.noreply.github.com>
2018-10-25 07:10:41 -0600

    Extract WinHttp code into mockable service, add Request/Response data structures (#148)

---
+++ src/services/impl/WindowsHttpRequester.hh
@@ -0,0 +1,30 @@
+#ifndef RA_SERVICES_WIN32_HTTPREQUESTER_HH
+#define RA_SERVICES_WIN32_HTTPREQUESTER_HH
+#pragma once
+
+#include "services\IHttpRequester.hh"
+
+#include "RA_StringUtils.h"
+
+namespace ra {
+namespace services {
+namespace impl {
+
+#undef CreateDirectory
+
+class WindowsHttpRequester : public IHttpRequester
+{
+public:
+    void SetUserAgent(const std::string& sUserAgent) override { m_sUserAgent = ra::Widen(sUserAgent); }
+
+    unsigned int Request(const Http::Request& pRequest, TextWriter& pContentWriter) const override;
+
+private:
+    std::wstring m_sUserAgent;
+};
+
+} // namespace impl
+} // namespace services
+} // namespace ra
+
+#endif // !RA_SERVICES_WIN32_HTTPREQUESTER_HH

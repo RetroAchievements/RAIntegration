67a18a5a912bcc96471d36b2d485b990e4cb3781
SyrianBallaS <scharif01@gmail.com>
2018-11-15 17:23:16 -0500

    Move rcheevos and other 3rd party libs (w/ source files) into another project.

---
+++ rcheevos_lib/stdafx.h
@@ -0,0 +1,23 @@
+#ifndef STDAFX_H
+#define STDAFX_H
+/***************************************************************************
+* stdafx.h : include file for standard system include files,
+*            or project specific include files that are used frequently, but
+*            are changed infrequently
+*/
+#pragma once
+
+#include <ctype.h>
+#include <errno.h>
+#include <float.h>
+#include <limits.h>
+#include <locale.h>
+#include <math.h>
+#include <setjmp.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#endif /* !STDAFX_H */

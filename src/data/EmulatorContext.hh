1694e1c0a11c6b1e1bbf91d2a8970cf130c2fb2f
Jamiras <jamirasmt@gmail.com>
2018-12-27 10:11:46 -0700

    add doc comments to EmulatorContext

--- src/data/EmulatorContext.hh
+++ src/data/EmulatorContext.hh
@@ -24,20 +24,55 @@ public:
     /// </summary>
     void Initialize(EmulatorID nEmulatorId);
 
+    /// <summary>
+    /// Sets the client version.
+    /// </summary>
     void SetClientVersion(const std::string& sVersion) { m_sVersion = sVersion; }
 
-    bool ValidateClientVersion();
-
-    EmulatorID GetEmulatorId() const noexcept { return m_nEmulatorId; }
-
-    const std::string& GetClientName() const noexcept { return m_sClientName; }
-
+    /// <summary>
+    /// Gets the version of the emulator this context was initialized with.
+    /// </summary>
     const std::string& GetClientVersion() const noexcept { return m_sVersion; }
 
+    /// <summary>
+    /// Validates the client version against the latest version known by the server
+    /// </summary>
+    /// <remarks>
+    /// May show dialogs prompting the user to acknowledge their version is out of date.
+    /// The first time this is called, it makes a synchronous server call.
+    /// </remarks>
+    /// <returns>
+    /// <c>true</c> if client version is sufficient for the current configuration, 
+    /// <c>false</c> if not.
+    /// </returns>
+    bool ValidateClientVersion();
+
+    /// <summary>
+    /// Gets the unique identifier of the emulator this context was initialized with.
+    /// </summary>
+    EmulatorID GetEmulatorId() const noexcept { return m_nEmulatorId; }
+
+    /// <summary>
+    /// Gets the client name for the emulator this context was initialized with.
+    /// </summary>
+    const std::string& GetClientName() const noexcept { return m_sClientName; }
+
+    /// <summary>
+    /// Disables hardcore mode and notifies other services of that fact.
+    /// </summary>
     void DisableHardcoreMode();
 
+    /// <summary>
+    /// Enables hardcore mode and notifies other services of that fact.
+    /// </summary>
+    /// <returns><c>true</c> if hardcore mode was enabled, <c>false</c> if the user chose not to based on prompts they
+    /// were given.</returns>
     bool EnableHardcoreMode();
 
+    /// <summary>
+    /// Builds a string for displaying in the title bar of the emulator.
+    /// </summary>
+    /// <param name="sMessage">A custom message provided by the emulator to also display in the title bar.</param>
     std::string GetAppTitle(const std::string& sMessage) const;
 
 protected:

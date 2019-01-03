de559b5cc3832040f0a94ac8e4271a6e6d19e094
SyrianBallaS <scharif01@gmail.com>
2019-01-02 22:18:50 -0500

    Don't cast pointers to the same type (type.1)

--- src/RA_md5factory.cpp
+++ src/RA_md5factory.cpp
@@ -47,7 +47,7 @@ std::string RAGenerateMD5(const BYTE* pRawData, size_t nDataLen)
     static_assert(sizeof(md5_byte_t) == sizeof(BYTE), "Must be equivalent for the MD5 to work!");
 
     md5_init(&pms);
-    md5_append(&pms, static_cast<const md5_byte_t*>(pRawData), nDataLen);
+    md5_append(&pms, pRawData, nDataLen);
     md5_finish(&pms, digest);
 
     memset(buffer, 0, MD5_STRING_LEN + 1);

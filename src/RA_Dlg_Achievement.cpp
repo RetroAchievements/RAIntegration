9c1b5fd8d9113410c63ff6c82b0227f0dc12b5ba
SyrianBallaS <scharif01@gmail.com>
2019-01-13 15:12:35 -0500

    CppCoreCheck Type Profile (es.47) (#245)

--- src/RA_Dlg_Achievement.cpp
+++ src/RA_Dlg_Achievement.cpp
@@ -1198,7 +1198,7 @@ void Dlg_Achievements::OnEditData(size_t nItem, Column nColumn, const std::strin
         else
         {
             RECT rcBounds{};
-            ListView_GetItemRect(hList, nItem, &rcBounds, LVIR_BOUNDS);
+            GSL_SUPPRESS_ES47 ListView_GetItemRect(hList, nItem, &rcBounds, LVIR_BOUNDS);
             InvalidateRect(hList, &rcBounds, FALSE);
         }
     }

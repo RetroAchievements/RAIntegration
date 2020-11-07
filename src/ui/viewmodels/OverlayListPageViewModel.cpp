89d42e1a05cdc6f4f46f23dd8839658c4f07dc59
Jamiras <32680403+Jamiras@users.noreply.github.com>
2020-11-07 07:44:31 -0800

    highlight recently earned achievements in overlay (#623)

--- src/ui/viewmodels/OverlayListPageViewModel.cpp
+++ src/ui/viewmodels/OverlayListPageViewModel.cpp
@@ -116,46 +116,56 @@ void OverlayListPageViewModel::RenderList(ra::ui::drawing::ISurface& pSurface, i
         if (!pItem)
             break;
 
-        auto nTextX = nX;
-        if (pItem->Image.Type() != ra::ui::ImageType::None)
+        if (pItem->IsHeader())
         {
-            pSurface.DrawImage(nX, nY, nItemSize, nItemSize, pItem->Image);
-            nTextX += nItemSize;
+            const auto sHeader = ra::StringPrintf(L"----- %s -----", pItem->GetLabel());
+            const auto szHeader = pSurface.MeasureText(nFont, sHeader);
+
+            pSurface.WriteText((nWidth - szHeader.Width) / 2, nY + nItemSize - szHeader.Height - 4, nFont, pTheme.ColorOverlayText(), sHeader);
         }
-
-        const ra::ui::Color nSubTextColor = pItem->IsDisabled() ? pTheme.ColorOverlayDisabledSubText() : pTheme.ColorOverlaySubText();
-        ra::ui::Color nTextColor = pTheme.ColorOverlayText();
-
-        const bool bSelected = (nIndex == ra::to_signed(nSelectedIndex));
-        if (bSelected)
+        else
         {
-            pSurface.FillRectangle(nTextX, nY, nWidth - nTextX, nItemSize, pTheme.ColorOverlaySelectionBackground());
-            nTextColor = pItem->IsDisabled() ?
-                pTheme.ColorOverlaySelectionDisabledText() : pTheme.ColorOverlaySelectionText();
-        }
-        else if (pItem->IsDisabled())
-        {
-            nTextColor = pTheme.ColorOverlayDisabledText();
-        }
+            auto nTextX = nX;
+            if (pItem->Image.Type() != ra::ui::ImageType::None)
+            {
+                pSurface.DrawImage(nX, nY, nItemSize, nItemSize, pItem->Image);
+                nTextX += nItemSize;
+            }
 
-        pSurface.WriteText(nTextX + 12, nY + 1, nFont, nTextColor, pItem->GetLabel());
-        pSurface.WriteText(nTextX + 12, nY + 1 + 26, nSubFont, nSubTextColor, pItem->GetDetail());
+            const ra::ui::Color nSubTextColor = pItem->IsDisabled() ? pTheme.ColorOverlayDisabledSubText() : pTheme.ColorOverlaySubText();
+            ra::ui::Color nTextColor = pTheme.ColorOverlayText();
 
-        const auto nTarget = pItem->GetProgressMaximum();
-        if (nTarget != 0)
-        {
-            const auto nProgressBarWidth = (nWidth - nTextX - 12) * 2 / 3;
-            const auto nValue = std::min(pItem->GetProgressValue(), nTarget);
-            const auto nProgressBarFillWidth = gsl::narrow_cast<int>(((static_cast<long>(nProgressBarWidth) - 2) * nValue) / nTarget);
-            const auto nProgressBarPercent = gsl::narrow_cast<int>(static_cast<long>(nValue) * 100 / nTarget);
+            const bool bSelected = (nIndex == ra::to_signed(nSelectedIndex));
+            if (bSelected)
+            {
+                pSurface.FillRectangle(nTextX, nY, nWidth - nTextX, nItemSize, pTheme.ColorOverlaySelectionBackground());
+                nTextColor = pItem->IsDisabled() ?
+                    pTheme.ColorOverlaySelectionDisabledText() : pTheme.ColorOverlaySelectionText();
+            }
+            else if (pItem->IsDisabled())
+            {
+                nTextColor = pTheme.ColorOverlayDisabledText();
+            }
 
-            pSurface.FillRectangle(nTextX + 12, nY + 1 + 26 + 25, nProgressBarWidth, 8, pTheme.ColorOverlayScrollBar());
-            pSurface.FillRectangle(nTextX + 12 + 2, nY + 1 + 26 + 25 + 2, nProgressBarFillWidth - 4, 8 - 4, pTheme.ColorOverlayScrollBarGripper());
+            pSurface.WriteText(nTextX + 12, nY + 1, nFont, nTextColor, pItem->GetLabel());
+            pSurface.WriteText(nTextX + 12, nY + 1 + 26, nSubFont, nSubTextColor, pItem->GetDetail());
 
-            const auto nProgressFont = pSurface.LoadFont(pTheme.FontOverlay(), 14, ra::ui::FontStyles::Normal);
-            const std::wstring sProgressPercent = ra::StringPrintf(L"%d%%", nProgressBarPercent);
-            const auto szProgressPercent = pSurface.MeasureText(nProgressFont, sProgressPercent);
-            pSurface.WriteText(nTextX + 12 + nProgressBarWidth + 6, nY + 1 + 26 + 25 + 4 - (szProgressPercent.Height / 2) - 1, nProgressFont, nSubTextColor, sProgressPercent);
+            const auto nTarget = pItem->GetProgressMaximum();
+            if (nTarget != 0)
+            {
+                const auto nProgressBarWidth = (nWidth - nTextX - 12) * 2 / 3;
+                const auto nValue = std::min(pItem->GetProgressValue(), nTarget);
+                const auto nProgressBarFillWidth = gsl::narrow_cast<int>(((static_cast<long>(nProgressBarWidth) - 2) * nValue) / nTarget);
+                const auto nProgressBarPercent = gsl::narrow_cast<int>(static_cast<long>(nValue) * 100 / nTarget);
+
+                pSurface.FillRectangle(nTextX + 12, nY + 1 + 26 + 25, nProgressBarWidth, 8, pTheme.ColorOverlayScrollBar());
+                pSurface.FillRectangle(nTextX + 12 + 2, nY + 1 + 26 + 25 + 2, nProgressBarFillWidth - 4, 8 - 4, pTheme.ColorOverlayScrollBarGripper());
+
+                const auto nProgressFont = pSurface.LoadFont(pTheme.FontOverlay(), 14, ra::ui::FontStyles::Normal);
+                const std::wstring sProgressPercent = ra::StringPrintf(L"%d%%", nProgressBarPercent);
+                const auto szProgressPercent = pSurface.MeasureText(nProgressFont, sProgressPercent);
+                pSurface.WriteText(nTextX + 12 + nProgressBarWidth + 6, nY + 1 + 26 + 25 + 4 - (szProgressPercent.Height / 2) - 1, nProgressFont, nSubTextColor, sProgressPercent);
+            }
         }
 
         nIndex++;
@@ -168,40 +178,51 @@ bool OverlayListPageViewModel::ProcessInput(const ControllerInput& pInput)
 {
     if (pInput.m_bDownPressed)
     {
-        const auto nSelectedItemIndex = GetSelectedItemIndex();
-        if (nSelectedItemIndex < ra::to_signed(m_vItems.Count()) - 1)
+        size_t nSelectedItemIndex = gsl::narrow_cast<size_t>(GetSelectedItemIndex());
+        if (nSelectedItemIndex < m_vItems.Count() - 1)
         {
-            SetSelectedItemIndex(nSelectedItemIndex + 1);
+            ++nSelectedItemIndex;
+            auto* vmItem = m_vItems.GetItemAt(nSelectedItemIndex);
 
-            if (gsl::narrow_cast<size_t>(nSelectedItemIndex) + 1 - m_nScrollOffset == gsl::narrow_cast<size_t>(m_nVisibleItems))
-                ++m_nScrollOffset;
+            // skip over header items
+            while (vmItem && vmItem->IsHeader())
+                vmItem = m_vItems.GetItemAt(++nSelectedItemIndex);
+
+            if (!vmItem)
+                return false;
+                
+            SetSelectedItemIndex(gsl::narrow_cast<int>(nSelectedItemIndex));
+
+            if (nSelectedItemIndex - m_nScrollOffset >= gsl::narrow_cast<size_t>(m_nVisibleItems))
+                m_nScrollOffset = nSelectedItemIndex - m_nVisibleItems + 1;
 
             if (m_bDetail)
-            {
-                auto* vmItem = m_vItems.GetItemAt(gsl::narrow_cast<size_t>(nSelectedItemIndex) + 1);
-                if (vmItem != nullptr)
-                    FetchItemDetail(*vmItem);
-            }
+                FetchItemDetail(*vmItem);
 
             return true;
         }
     }
     else if (pInput.m_bUpPressed)
     {
-        const auto nSelectedItemIndex = GetSelectedItemIndex();
+        auto nSelectedItemIndex = GetSelectedItemIndex();
         if (nSelectedItemIndex > 0)
         {
-            SetSelectedItemIndex(nSelectedItemIndex - 1);
+            auto* vmItem = m_vItems.GetItemAt(gsl::narrow_cast<size_t>(--nSelectedItemIndex));
 
-            if (nSelectedItemIndex == m_nScrollOffset)
-                --m_nScrollOffset;
+            // skip over header items
+            while (vmItem && vmItem->IsHeader())
+                vmItem = m_vItems.GetItemAt(gsl::narrow_cast<size_t>(--nSelectedItemIndex));
+
+            if (nSelectedItemIndex < m_nScrollOffset)
+                m_nScrollOffset = std::max(nSelectedItemIndex, 0);
+
+            if (!vmItem)
+                return false;
+
+            SetSelectedItemIndex(nSelectedItemIndex);
 
             if (m_bDetail)
-            {
-                auto* vmItem = m_vItems.GetItemAt(gsl::narrow_cast<size_t>(nSelectedItemIndex) - 1);
-                if (vmItem != nullptr)
-                    FetchItemDetail(*vmItem);
-            }
+                FetchItemDetail(*vmItem);
 
             return true;
         }

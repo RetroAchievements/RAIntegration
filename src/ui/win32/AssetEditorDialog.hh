790e34b0030eb45820eba5f1f4c1137a1c7f1405
Jamiras <jamirasmt@gmail.com>
2021-05-23 08:33:38 -0600

    GetStockIconInfo is not available on WinXP

--- src/ui/win32/AssetEditorDialog.hh
+++ src/ui/win32/AssetEditorDialog.hh
@@ -31,6 +31,8 @@ public:
     class Presenter : public IClosableDialogPresenter
     {
     public:
+        Presenter() noexcept;
+
         bool IsSupported(const ra::ui::WindowViewModelBase& viewModel) noexcept override;
         void ShowWindow(ra::ui::WindowViewModelBase& viewModel) override;
         void ShowModal(ra::ui::WindowViewModelBase& viewModel, HWND hParentWnd) override;

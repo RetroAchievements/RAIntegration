69f7941042f9b47e43f6ca9fea268ec04af581db
Jamiras <32680403+Jamiras@users.noreply.github.com>
2021-10-30 10:57:22 -0600

    use rcheevos for trigger validation (#818)

--- tests/data/models/TriggerValidation_Tests.cpp
+++ tests/data/models/TriggerValidation_Tests.cpp
@@ -35,16 +35,16 @@ public:
 
     TEST_METHOD(TestLastConditionCombining)
     {
-        AssertValidation("0xH1234=1_A:0xH2345=2", L"Final condition type expects another condition to follow.");
-        AssertValidation("0xH1234=1_B:0xH2345=2", L"Final condition type expects another condition to follow.");
-        AssertValidation("0xH1234=1_C:0xH2345=2", L"Final condition type expects another condition to follow.");
-        AssertValidation("0xH1234=1_D:0xH2345=2", L"Final condition type expects another condition to follow.");
-        AssertValidation("0xH1234=1_N:0xH2345=2", L"Final condition type expects another condition to follow.");
-        AssertValidation("0xH1234=1_O:0xH2345=2", L"Final condition type expects another condition to follow.");
-        AssertValidation("0xH1234=1_Z:0xH2345=2", L"Final condition type expects another condition to follow.");
+        AssertValidation("0xH1234=1_A:0xH2345=2", L"Final condition type expects another condition to follow");
+        AssertValidation("0xH1234=1_B:0xH2345=2", L"Final condition type expects another condition to follow");
+        AssertValidation("0xH1234=1_C:0xH2345=2", L"Final condition type expects another condition to follow");
+        AssertValidation("0xH1234=1_D:0xH2345=2", L"Final condition type expects another condition to follow");
+        AssertValidation("0xH1234=1_N:0xH2345=2", L"Final condition type expects another condition to follow");
+        AssertValidation("0xH1234=1_O:0xH2345=2", L"Final condition type expects another condition to follow");
+        AssertValidation("0xH1234=1_Z:0xH2345=2", L"Final condition type expects another condition to follow");
 
-        AssertValidation("0xH1234=1_A:0xH2345=2S0x3456=1", L"Core: Final condition type expects another condition to follow.");
-        AssertValidation("0x3456=1S0xH1234=1_A:0xH2345=2", L"Alt 1: Final condition type expects another condition to follow.");
+        AssertValidation("0xH1234=1_A:0xH2345=2S0x3456=1", L"Core Final condition type expects another condition to follow");
+        AssertValidation("0x3456=1S0xH1234=1_A:0xH2345=2", L"Alt1 Final condition type expects another condition to follow");
     }
 
     TEST_METHOD(TestMiddleConditionCombining)
@@ -58,8 +58,8 @@ public:
 
     TEST_METHOD(TestAddHitsChainWithoutTarget)
     {
-        AssertValidation("C:0xH1234=1_0xH2345=2", L"Condition 2: Final condition in AddHits chain must have a hit target.");
-        AssertValidation("D:0xH1234=1_0xH2345=2", L"Condition 2: Final condition in AddHits chain must have a hit target.");
+        AssertValidation("C:0xH1234=1_0xH2345=2", L"Condition 2: Final condition in AddHits chain must have a hit target");
+        AssertValidation("D:0xH1234=1_0xH2345=2", L"Condition 2: Final condition in AddHits chain must have a hit target");
         AssertValidation("C:0xH1234=1_0xH2345=2.1.", L"");
         AssertValidation("D:0xH1234=1_0xH2345=2.1.", L"");
     }
@@ -128,9 +128,9 @@ public:
 
         // basic checks for each side
         AssertValidation("0xH1234>0xH1235", L"");
-        AssertValidation("0xH12345>0xH1235", L"Condition 1: Address out of range (max FFFF).");
-        AssertValidation("0xH1234>0xH12345", L"Condition 1: Address out of range (max FFFF).");
-        AssertValidation("0xH12345>0xH12345", L"Condition 1: Address out of range (max FFFF).");
+        AssertValidation("0xH12345>0xH1235", L"Condition 1: Address 12345 out of range (max FFFF)");
+        AssertValidation("0xH1234>0xH12345", L"Condition 1: Address 12345 out of range (max FFFF)");
+        AssertValidation("0xH12345>0xH12345", L"Condition 1: Address 12345 out of range (max FFFF)");
         AssertValidation("0xX1234>h12345", L"");
 
         // support for multiple memory blocks and edge addresses
@@ -139,11 +139,11 @@ public:
         AssertValidation("0xH12345>0xH1235", L"");
         AssertValidation("0xH0000>5", L"");
         AssertValidation("0xH1FFFF>5", L"");
-        AssertValidation("0xH20000>5", L"Condition 1: Address out of range (max 1FFFF).");
+        AssertValidation("0xH20000>5", L"Condition 1: Address 20000 out of range (max 1FFFF)");
 
         // AddAddress can use really big values for negative offsets, don't flag them.
         AssertValidation("I:0xX1234_0xHFFFFFF00>5", L"");
-        AssertValidation("I:0xX1234_0xH1234>5_0xHFFFFFF00>5", L"Condition 3: Address out of range (max 1FFFF).");
+        AssertValidation("I:0xX1234_0xH1234>5_0xHFFFFFF00>5", L"Condition 3: Address FFFFFF00 out of range (max 1FFFF)");
     }
 };
 
--- tests/ui/viewmodels/AssetListViewModel_Tests.cpp
+++ tests/ui/viewmodels/AssetListViewModel_Tests.cpp
@@ -1047,7 +1047,7 @@ public:
         vmAchievement.SetTrigger("A:0x1234");
         vmAchievement.UpdateLocalCheckpoint();
         Assert::IsFalse(vmAchievement.Validate());
-        Assert::AreEqual(std::wstring(L"Final condition type expects another condition to follow."), pItem->GetWarning());
+        Assert::AreEqual(std::wstring(L"Final condition type expects another condition to follow"), pItem->GetWarning());
     }
 
     TEST_METHOD(TestSyncAddItem)
@@ -2002,7 +2002,7 @@ public:
         const auto& sText = vmAssetList.GetUserFile(L"22");
         AssertContains(sText, "1:\"A:0x1234\":Test1b:Desc1:::::5:::::12345");
 
-        Assert::AreEqual(std::wstring(L"Final condition type expects another condition to follow."), pItem->GetValidationError());
+        Assert::AreEqual(std::wstring(L"Final condition type expects another condition to follow"), pItem->GetValidationError());
 
         pItem->SetName(L"Test1c");
         vmAssetList.SaveSelected();
@@ -2139,7 +2139,7 @@ public:
         pItem->SetTrigger("A:0x1234");
         pItem->UpdateLocalCheckpoint();
         Assert::AreEqual(AssetChanges::Unpublished, pItem->GetChanges());
-        Assert::AreEqual(std::wstring(L"Final condition type expects another condition to follow."), pItem->GetValidationError());
+        Assert::AreEqual(std::wstring(L"Final condition type expects another condition to follow"), pItem->GetValidationError());
 
         vmAssetList.FilteredAssets().GetItemAt(0)->SetSelected(true);
         vmAssetList.ForceUpdateButtons();
@@ -2151,7 +2151,7 @@ public:
         Assert::AreEqual({ 1U }, vmAssetList.PublishedAssets.size());
         Assert::IsTrue(pItem == dynamic_cast<ra::data::models::AchievementModel*>(vmAssetList.PublishedAssets.at(0)));
         Assert::AreEqual(AssetChanges::None, pItem->GetChanges());
-        Assert::AreEqual(std::wstring(L"Final condition type expects another condition to follow."), pItem->GetValidationError());
+        Assert::AreEqual(std::wstring(L"Final condition type expects another condition to follow"), pItem->GetValidationError());
 
         vmAssetList.ForceUpdateButtons();
         vmAssetList.AssertButtonState(SaveButtonState::Demote);

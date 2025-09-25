0a205e440b507cc42848302fa92c6fe0a1c8f686
Jamiras <32680403+Jamiras@users.noreply.github.com>
2025-09-25 07:37:22 -0600

    use mutex when sync'ing state (#1235)

--- src/data/models/AchievementModel.cpp
+++ src/data/models/AchievementModel.cpp
@@ -251,6 +251,11 @@ void AchievementModel::HandleStateChanged(AssetState nOldState, AssetState nNewS
 
 void AchievementModel::SyncState()
 {
+    auto& pRuntime = ra::services::ServiceLocator::GetMutable<ra::services::AchievementRuntime>();
+    auto* pClient = pRuntime.GetClient();
+
+    rc_mutex_lock(&pClient->state.mutex);
+
     switch (GetState())
     {
         case ra::data::models::AssetState::Triggered: {
@@ -263,7 +268,6 @@ void AchievementModel::SyncState()
                 const auto& pClock = ra::services::ServiceLocator::Get<ra::services::IClock>();
                 m_tUnlock = pClock.Now();
 
-                auto& pRuntime = ra::services::ServiceLocator::GetMutable<ra::services::AchievementRuntime>();
                 if (pRuntime.HasRichPresence())
                     SetUnlockRichPresence(pRuntime.GetRichPresenceDisplayString());
             }
@@ -305,6 +309,8 @@ void AchievementModel::SyncState()
             m_pAchievement->public_.state = RC_CLIENT_ACHIEVEMENT_STATE_ACTIVE;
             break;
     }
+
+    rc_mutex_unlock(&pClient->state.mutex);
 }
 
 void AchievementModel::SyncID()
@@ -424,8 +430,11 @@ void AchievementModel::SyncTrigger()
             return;
         }
 
-        auto* pGame = pRuntime.GetClient()->game;
+        auto* pClient = pRuntime.GetClient();
+        auto* pGame = pClient->game;
         Expects(pGame != nullptr);
+
+        rc_mutex_lock(&pClient->state.mutex);
 
         rc_preparse_state_t preparse;
         rc_init_preparse_state(&preparse);
@@ -473,6 +482,8 @@ void AchievementModel::SyncTrigger()
                 }
             }
         }
+
+        rc_mutex_unlock(&pClient->state.mutex);
 
         rc_destroy_preparse_state(&preparse);
     }

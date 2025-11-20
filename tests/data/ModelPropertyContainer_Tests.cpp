1d58ef6604bf01e9344c4c24be617a5821ecad1e
Jamiras <jamirasmt@gmail.com>
2025-11-20 15:36:32 -0700

    add more tests

--- tests/data/ModelPropertyContainer_Tests.cpp
+++ tests/data/ModelPropertyContainer_Tests.cpp
@@ -11,15 +11,15 @@ TEST_CLASS(ModelPropertyContainer_Tests)
     class ModelPropertyContainerHarness : public ModelPropertyContainer
     {
     public:
-        StringModelProperty StringProperty{ "ViewModelHarness", "String", L"" };
+        StringModelProperty StringProperty{ "ModelPropertyContainerHarness", "String", L"" };
         const std::wstring& GetString() const { return GetValue(StringProperty); }
         void SetString(const std::wstring& sValue) { SetValue(StringProperty, sValue); }
 
-        IntModelProperty IntProperty{ "ViewModelHarness", "Int", 0 };
+        IntModelProperty IntProperty{ "ModelPropertyContainerHarness", "Int", 0 };
         int GetInt() const { return GetValue(IntProperty); }
         void SetInt(int nValue) { SetValue(IntProperty, nValue); }
 
-        BoolModelProperty BoolProperty{ "ViewModelHarness", "Bool", false };
+        BoolModelProperty BoolProperty{ "ModelPropertyContainerHarness", "Bool", false };
         bool GetBool() const { return GetValue(BoolProperty); }
         void SetBool(bool bValue) { SetValue(BoolProperty, bValue); }
     };
@@ -27,29 +27,119 @@ TEST_CLASS(ModelPropertyContainer_Tests)
 public:
     TEST_METHOD(TestStringProperty)
     {
-        ModelPropertyContainerHarness vmViewModel;
-        Assert::AreEqual(std::wstring(), vmViewModel.GetString());
+        ModelPropertyContainerHarness container;
+        Assert::AreEqual(std::wstring(), container.GetString());
 
-        vmViewModel.SetString(L"Test");
-        Assert::AreEqual(std::wstring(L"Test"), vmViewModel.GetString());
+        container.SetString(L"Test");
+        Assert::AreEqual(std::wstring(L"Test"), container.GetString());
+
+        container.SetString(L"Test2");
+        Assert::AreEqual(std::wstring(L"Test2"), container.GetString());
+
+        container.SetString(container.StringProperty.GetDefaultValue());
+        Assert::AreEqual(std::wstring(), container.GetString());
     }
 
     TEST_METHOD(TestIntProperty)
     {
-        ModelPropertyContainerHarness vmViewModel;
-        Assert::AreEqual(0, vmViewModel.GetInt());
+        ModelPropertyContainerHarness container;
+        Assert::AreEqual(0, container.GetInt());
 
-        vmViewModel.SetInt(32);
-        Assert::AreEqual(32, vmViewModel.GetInt());
+        container.SetInt(32);
+        Assert::AreEqual(32, container.GetInt());
+
+        container.SetInt(64);
+        Assert::AreEqual(64, container.GetInt());
+
+        container.SetInt(-1);
+        Assert::AreEqual(-1, container.GetInt());
+
+        container.SetInt(container.IntProperty.GetDefaultValue());
+        Assert::AreEqual(0, container.GetInt());
     }
 
     TEST_METHOD(TestBoolProperty)
     {
-        ModelPropertyContainerHarness vmViewModel;
-        Assert::AreEqual(false, vmViewModel.GetBool());
+        ModelPropertyContainerHarness container;
+        Assert::AreEqual(false, container.GetBool());
 
-        vmViewModel.SetBool(true);
-        Assert::AreEqual(true, vmViewModel.GetBool());
+        container.SetBool(true);
+        Assert::AreEqual(true, container.GetBool());
+
+        container.SetBool(container.BoolProperty.GetDefaultValue());
+        Assert::AreEqual(false, container.GetBool());
+    }
+
+    TEST_METHOD(TestSetAllInOrder)
+    {
+        ModelPropertyContainerHarness container;
+        Assert::AreEqual(std::wstring(), container.GetString());
+        Assert::AreEqual(0, container.GetInt());
+        Assert::AreEqual(false, container.GetBool());
+
+        container.SetString(L"Test");
+        container.SetInt(32);
+        container.SetBool(true);
+
+        Assert::AreEqual(std::wstring(L"Test"), container.GetString());
+        Assert::AreEqual(32, container.GetInt());
+        Assert::AreEqual(true, container.GetBool());
+
+        container.SetString(container.StringProperty.GetDefaultValue());
+        container.SetInt(container.IntProperty.GetDefaultValue());
+        container.SetBool(container.BoolProperty.GetDefaultValue());
+
+        Assert::AreEqual(std::wstring(), container.GetString());
+        Assert::AreEqual(0, container.GetInt());
+        Assert::AreEqual(false, container.GetBool());
+    }
+
+    TEST_METHOD(TestSetAllInReverseOrder)
+    {
+        ModelPropertyContainerHarness container;
+        Assert::AreEqual(std::wstring(), container.GetString());
+        Assert::AreEqual(0, container.GetInt());
+        Assert::AreEqual(false, container.GetBool());
+
+        container.SetBool(true);
+        container.SetInt(32);
+        container.SetString(L"Test");
+
+        Assert::AreEqual(std::wstring(L"Test"), container.GetString());
+        Assert::AreEqual(32, container.GetInt());
+        Assert::AreEqual(true, container.GetBool());
+
+        container.SetBool(container.BoolProperty.GetDefaultValue());
+        container.SetInt(container.IntProperty.GetDefaultValue());
+        container.SetString(container.StringProperty.GetDefaultValue());
+
+        Assert::AreEqual(std::wstring(), container.GetString());
+        Assert::AreEqual(0, container.GetInt());
+        Assert::AreEqual(false, container.GetBool());
+    }
+
+    TEST_METHOD(TestSetAllInZigZagOrder)
+    {
+        ModelPropertyContainerHarness container;
+        Assert::AreEqual(std::wstring(), container.GetString());
+        Assert::AreEqual(0, container.GetInt());
+        Assert::AreEqual(false, container.GetBool());
+
+        container.SetInt(32);
+        container.SetString(L"Test");
+        container.SetBool(true);
+
+        Assert::AreEqual(std::wstring(L"Test"), container.GetString());
+        Assert::AreEqual(32, container.GetInt());
+        Assert::AreEqual(true, container.GetBool());
+
+        container.SetInt(container.IntProperty.GetDefaultValue());
+        container.SetString(container.StringProperty.GetDefaultValue());
+        container.SetBool(container.BoolProperty.GetDefaultValue());
+
+        Assert::AreEqual(std::wstring(), container.GetString());
+        Assert::AreEqual(0, container.GetInt());
+        Assert::AreEqual(false, container.GetBool());
     }
 };
 
